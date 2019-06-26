1298c955e2c252e4b4bc7a1a6dad113254cc8a2f
v715 <vgopala4@jhu.edu>
2019-06-26 12:36:39 -0400

    Remove apostrophes to fix SyntaxError

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Experiments showing the "Do's and Don'ts" of connectome analysis',
+    description='Experiments showing the "Dos and Donts" of connectome analysis',
     author='Neurodata',
     license='BSD-3',
 )
