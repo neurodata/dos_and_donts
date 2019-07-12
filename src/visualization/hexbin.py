b589acc601ff9eabdb63776ee435449d54dcd72f
j1c <jaewonc78@gmail.com>
2019-07-12 14:40:19 -0400

    update hexbin

--- src/visualization/hexbin.py
+++ src/visualization/hexbin.py
@@ -26,40 +26,41 @@ def hexbin(
     df,
     ordering,
     x_col="modularity",
-    figsize=(18, 16),
     gridsize=40,
     cmap="Blues",
     bins="log",
     title=None,
     savefig=None,
 ):
+    xlabel = ordering[x_col]
+    ordering = {key: val for key, val in ordering.items() if key != x_col}
+
     ncols = 3
     nrows = int(np.ceil(len(ordering) / ncols))
+    figsize = (4 * nrows, 3 * ncols)
 
     fig, ax = plt.subplots(ncols=ncols, nrows=nrows, figsize=figsize, sharex=True)
     ax = ax.ravel()
 
     for idx, (col, y_label) in enumerate(ordering.items()):
-        ax[idx].hexbin(
-            x=df.modularity, y=df[col], cmap=cmap, gridsize=gridsize, bins=bins
-        )
+        ax[idx].hexbin(x=df[x_col], y=df[col], cmap=cmap, gridsize=gridsize, bins=bins)
         sns.despine(ax=ax[idx])
         # ax[idx].set_xlabel('Modularity', fontsize=20)
         ax[idx].set_ylabel(y_label, fontsize=20)
 
     for i in range(1, 4):
-        ax[-i].set_xlabel(ordering[x_col], fontsize=20)
+        ax[-i].set_xlabel(xlabel, fontsize=20)
 
     if len(ordering) != (ncols * nrows):
         for i in range(1, len(ax) - len(ordering) + 1):
             fig.delaxes(ax[-i])
 
-    if title is not None:
-        ax[1].set_title(title, fontsize=30)
-
     fig.tight_layout()
 
+    if title is not None:
+        fig.suptitle(title, y=1.02, fontsize=30)
+
     if savefig is not None:
-        fig.savefig(f"{savefig}.png", dpi=300)
+        fig.savefig(f"{savefig}.png", dpi=300, bbox_inches="tight")
 
     plt.close()
