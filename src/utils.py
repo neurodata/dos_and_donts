08417fd45caa3a05d58b73f1fa727e6e86a1eab2
j1c <jaewonc78@gmail.com>
2020-02-13 11:38:36 -0500

    new pr function

--- src/utils.py
+++ src/utils.py
@@ -72,14 +72,17 @@ def estimate_block_probabilities(X, Y, labels):
     return bhat_1, bhat_2
 
 
-def compute_pr_at_k(pvalues, k, true_labels):
+def compute_pr_at_k(k, true_labels, test_statistics=None, pvalues=None):
     """
-    Computes precision and recall at various k. 
+    Computes precision and recall at various k. Can compute based on p-values
+    or test-statistics, but not both. 
 
     Parameters
     ----------
+    test_statistics : 2d-array like, shape (n, n)
+        Test-statistics obtained from some test.
     pvalues : 2d-array like, shape (n, n)
-        P-values obtained from some test.
+        P-values obtained from some test. 
     k : int or array-like
         Values @k to compute precision and recall for. If list, compute P/R for
         each value in list. Otherwise, it computes P/R from range(1, k+1).
@@ -91,15 +94,27 @@ def compute_pr_at_k(pvalues, k, true_labels):
     precisions, recalls : array-like
         Computed precisions and recalls.
     """
+    if (test_statistics is not None) and (pvalues is not None):
+        raise ValueError("You cannot supply both `test_statistics` and `pvalues`.")
+
+    if test_statistics is not None:
+        res = test_statistics
+        reverse_sorting = True
+    else:
+        res = pvalues
+        reverse_sorting = False
+
     label_matrix = np.zeros((len(true_labels), len(true_labels)))
     c1 = (true_labels == 0).sum()
     label_matrix[:c1, :c1] = 1
 
-    triu_idx = np.triu_indices_from(pvalues, k=1)
+    triu_idx = np.triu_indices_from(res, k=1)
     labels_vec = label_matrix[triu_idx]
-    pvals_vec = pvalues[triu_idx]
+    res_vec = res[triu_idx]
 
-    idx = np.argsort(pvals_vec)
+    idx = np.argsort(res_vec)
+    if reverse_sorting:
+        idx = idx[::-1]
     sorted_labels = labels_vec[idx]
 
     if isinstance(k, int):
