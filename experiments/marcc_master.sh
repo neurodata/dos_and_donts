3b10bc3f34fd23cea06218007a681db6a505e2b9
j1c <jaewonc78@gmail.com>
2020-03-01 16:47:46 -0500

    Add timer and reduce time

--- experiments/marcc_master.sh
+++ experiments/marcc_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=dnd
-#SBATCH --time=3-0:0:0
+#SBATCH --time=1-0:0:0
 #SBATCH --ntasks-per-node=48	
 #SBATCH --mem=200G
 #SBATCH --partition=lrgmem
@@ -11,6 +11,11 @@
 module load python/3.7
 source ~/dos_and_donts/venv/bin/activate
 
+start=`date +%s`
+
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 python3 $1 $SLURM_ARRAY_TASK_ID
 echo "job complete"
+
+end=`date +%s`
+echo Execution time was `expr $end - $start` seconds.
